488f6607f4609412acf5562b33cb3444b9305a22
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2022-12-31 16:34:30 -0500

    summary form display

--- app/javascript/react/components/SummaryForm.js
+++ app/javascript/react/components/SummaryForm.js
@@ -1,18 +1,52 @@
 import React, { useState } from "react"
 
 const SummaryForm = (props) => {
+  const clearState = {
+    title: "",
+    body: "",
+    api_doi: ""
+  }
+
+  const [errors, setErrors] = useState({})
+  const [user, setUser] = useState("")
+
+  const [summaryRecord, setSummaryRecord] = useState(clearState)
+
+
   return(
-    <div>Summary Form
-      <br />
-    Title:
-    Genre:
-    YEar:
-    Doi:
-    Journal name:
-    Authors:
-    URL For Landing Page:
-    URL For pdf:
-    </div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        // errors
+        <legend>Add summary:</legend>
+        <label htmlFor="title">
+          <input
+            id="title" 
+            name="title" 
+            onChange={handleInputChange} 
+            value={summaryRecord.title}
+            /> 
+        </label>
+
+        <label htmlFor="body">
+          <textarea
+            id="body" 
+            name="body" 
+            onChange={handleInputChange} 
+            value={summaryRecord.body}
+            /> 
+        </label>
+
+        <label htmlFor="api_doi">
+          <input
+            id="api_doi" 
+            name="api_doi" 
+            onChange={handleInputChange} 
+            value={summaryRecord.api_doi}
+            /> 
+        </label>
+        <input type="submit" value="Add your summary"/>
+      </form>
+      </div>
   )
 }
 
